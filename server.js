0b0ccbc14661943533d6e14498585402601b5803
gautam-jasmeet <jasmeet.gautam.solar@gmail.com>
2024-10-15 14:37:09 +0530

    testing

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to Document App</h1>");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`.bgCyan.white);
