78754ffda612064f969b338ad84ce18dbc5ef8a6
gautam-jasmeet <jasmeet.gautam.solar@gmail.com>
2024-10-16 13:06:32 +0530

    cors error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import hrRoutes from "./routes/hrRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" })); // Allow requests from any origin
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,12 +20,12 @@ app.use("/joining", joiningRoutes); // Use joining routes
 app.use("/hr", hrRoutes);
 app.use("/uploads", express.static("uploads"));
 
-//rest api
+// REST API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Document App</h1>");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`.bgCyan.white);
